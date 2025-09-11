eb891aea81394bb81d1d349b4888a9ec9f62a2c4
jooho <give928@gmail.com>
2025-09-11 16:51:57 +0900

    refactor: base model

--- common/models.py
+++ common/models.py
@@ -3,11 +3,22 @@ from django.db import models
 from accounts.models import User
 
 
-class Base(models.Model):
-    created_user = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='created_user_id', verbose_name='등록자')
+class CreatedBase(models.Model):
+    created_user = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='+', verbose_name='등록자')
     created_date = models.DateTimeField(auto_now_add=True, verbose_name='등록일시')
-    last_modified_user = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='last_modified_user_id', verbose_name='수정자')
+
+    class Meta:
+        abstract = True
+
+
+class LastModifiedBase(models.Model):
+    last_modified_user = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='+', verbose_name='수정자')
     last_modified_date = models.DateTimeField(auto_now=True, verbose_name='수정일시')
 
+    class Meta:
+        abstract = True
+
+
+class Base(CreatedBase, LastModifiedBase):
     class Meta:
         abstract = True
