17ab6fb72a4e6cda50106c4384edffebe744fbf7
jooho <give928@gmail.com>
2025-09-15 10:47:28 +0900

    feat: multi line description

--- rooms/forms.py
+++ rooms/forms.py
@@ -4,6 +4,15 @@ from .models import Room
 
 
 class RoomForm(forms.ModelForm):
+    description = forms.CharField(
+        label='설명',
+        widget=forms.Textarea(attrs={
+            'class': 'form-control h-25',
+            'maxlength': 1024,
+            'placeholder': '설명'
+        })
+    )
+
     class Meta:
         model = Room
         fields = ('name', 'description', 'capacity', 'has_monitor', 'has_microphone', 'is_active')
